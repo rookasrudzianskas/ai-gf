00efdcce2921248da696818301bc0a04f1bd8bd1
rokas <rookas.rudzenskas@gmail.com>
2025-01-07 13:36:12 +0200

    fix

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,14 @@
 import '../../global.css';
 
 import { Stack } from 'expo-router';
+import { StatusBar } from "expo-status-bar";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function Layout() {
-  return <Stack />;
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Stack />
+      <StatusBar style="auto" />
+    </GestureHandlerRootView>
+  )
 }
